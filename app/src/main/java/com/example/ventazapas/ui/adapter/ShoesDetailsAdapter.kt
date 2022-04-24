9043c529fd23d338186d349f4ed1b8b6a7825724
KevinMaggio <kevin.maggio@mercadolibre.com>
2022-04-24 14:03:20 -0300

    added carousel

--- app/src/main/java/com/example/ventazapas/ui/adapter/ShoesDetailsAdapter.kt
+++ app/src/main/java/com/example/ventazapas/ui/adapter/ShoesDetailsAdapter.kt
@@ -4,30 +4,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.ventazapas.R
 import com.example.ventazapas.databinding.ItemDetailsBinding
 import com.squareup.picasso.Picasso
 
-class ShoesDetailsAdapter (private val list : List<String>): RecyclerView.Adapter <ShoesDetailsHolder> () {
+class ShoesDetailsAdapter (private val list : List<String>): RecyclerView.Adapter <ShoesDetailsAdapter.ShoesDetailsHolder> () {
+class ShoesDetailsHolder(val binding: ItemDetailsBinding) : RecyclerView.ViewHolder(binding.root)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShoesDetailsHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_details, parent, false)
-        return ShoesDetailsHolder(view)
+        val binding = ItemDetailsBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ShoesDetailsHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ShoesDetailsHolder, position: Int) {
-        holder.render(list[position])
+        val shoes = list[position]
+        //Picasso.get().load(shoes).into(holder.binding.ivDetails)
+        Glide.with(holder.binding.ivDetails).load(shoes).into(holder.binding.ivDetails)
     }
 
     override fun getItemCount(): Int {
         return list.size
-    }
-
-}
-class ShoesDetailsHolder (view:View) : RecyclerView.ViewHolder(view){
-
-    private val binding = ItemDetailsBinding.bind(view)
-
-    fun render(image:String){
-        Picasso.get().load(image).into(binding.ivDetails)
     }
 }
